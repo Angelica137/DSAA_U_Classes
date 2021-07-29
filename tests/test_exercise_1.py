8d9f67dad6793b20ee32b43f3225eae6492349a5
Angelica <Angelica137@users.noreply.github.com>
2021-07-29 20:34:08 +0100

    get_most_common_month returns August, red

--- tests/test_exercise_1.py
+++ tests/test_exercise_1.py
@@ -1,4 +1,4 @@
-from scripts.exercise_1 import create_person_objects, get_april_birthdays
+from scripts.exercise_1 import create_person_objects, get_april_birthdays, get_most_common_month
 
 names = ['Howard', 'Richard', 'Jules', 'Trula', 'Michael', 'Elizabeth', 'Richard', 'Shirley', 'Mark', 'Brianna', 'Kenneth', 'Gwen', 'William', 'Rosa', 'Denver', 'Shelly', 'Sammy', 'Maryann', 'Kathleen', 'Andrew', 'Joseph', 'Kathleen', 'Lisa', 'Viola', 'George', 'Bonnie', 'Robert', 'William', 'Sabrina', 'John', 'Robert', 'Gil', 'Calvin', 'Robert', 'Dusty', 'Dario', 'Joeann', 'Terry', 'Alan', 'Rosa', 'Jeane', 'James', 'Rachel', 'Tu', 'Chelsea', 'Andrea', 'Ernest', 'Erica', 'Priscilla', 'Carol',
          'Michael', 'Dale', 'Arthur', 'Helen', 'James', 'Donna', 'Patricia', 'Betty', 'Patricia', 'Mollie', 'Nicole', 'Ernest', 'Wendy', 'Graciela', 'Teresa', 'Nicole', 'Trang', 'Caleb', 'Robert', 'Paul', 'Nieves', 'Arleen', 'Milton', 'James', 'Lawrence', 'Edward', 'Susan', 'Patricia', 'Tana', 'Jessica', 'Suzanne', 'Darren', 'Arthur', 'Holly', 'Mary', 'Randal', 'John', 'Laura', 'Betty', 'Chelsea', 'Margaret', 'Angel', 'Jeffrey', 'Mary', 'Donald', 'David', 'Roger', 'Evan', 'Danny', 'William']
@@ -14,3 +14,7 @@ people = create_person_objects(names, ages, months)
 def test_get_april_birthday_returns_april_bds():
     assert get_april_birthdays(people) == {'Michael': 11, 'Erica': 72, 'Carol': 36, 'Lisa': 37,
                                            'Lawrence': 87, 'Joseph': 25, 'Margaret': 35, 'Andrew': 13, 'Dusty': 53, 'Robert': 89}
+
+
+def test_get_most_common_month_returns_most_common_month():
+    assert get_most_common_month(people) == "August"
